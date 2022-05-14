bd6cae12a154d6b9bc343ddc2e38780cdc8462c4
arma7x <arma7x@live.com>
2022-05-14 18:04:48 +0800

    searchable product list

--- src/api.ts
+++ src/api.ts
@@ -1,9 +1,11 @@
 export class FakeStore {
 
+  // eslint-disable-next-line
   static getProducts(): Promise<any> {
     return fetch('https://fakestoreapi.com/products').then(res=>res.json());
   }
 
+  // eslint-disable-next-line
   static getProduct(id: string): Promise<any> {
     return fetch(`https://fakestoreapi.com/products/${id}`).then(res=>res.json());
   }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,16 @@
 <template>
   <div class="container">
     <div class="container">
-      <button class="btn btn-sm btn-primary mb-2" @click="fetchProducts">Refresh</button>
+      <div class="row d-flex justify-content-between">
+        <div class="col mb-2">
+          <button class="btn btn-primary" @click="fetchProducts">Refresh</button>
+        </div>
+        <div class="col mb-2 d-grid d-flex justify-content-end">
+          <div class="d-flex m-0">
+            <input class="form-control" type="search" placeholder="Search" aria-label="Search" v-model="searchText" @input="searchProducts">
+          </div>
+        </div>
+      </div>
       <div class="row justify-content-md-center">
         <div v-for="product in products" :key="product.id" class="col col-12 col-sm-6 col-md-4 col-lg-3 mb-3">
           <ProductCard :id="product.id" :title="product.title" :image="product.image" :price="product.price" />
@@ -25,7 +34,9 @@ export default defineComponent({
   },
   data() {
     return {
-      products: [],
+      searchText: '',
+      searchT: -1,
+      products: []
     }
   },
   mounted() {
@@ -38,6 +49,8 @@ export default defineComponent({
   },
   methods: {
     fetchProducts() {
+      if (store.state.loading)
+        return
       store.commit('setLoading', true)
       this.products = []
       FakeStore.getProducts()
@@ -51,6 +64,33 @@ export default defineComponent({
       .finally(() => {
         store.commit('setLoading', false)
       })
+    },
+    searchProducts() {
+      if (this.searchT > 0) {
+        clearTimeout(this.searchT)
+        this.searchT = -1
+      }
+      this.searchT = setTimeout(() => {
+        if (store.state.products.length > 0 && this.searchText.length > 0) {
+          const word = this.searchText.toLowerCase()
+          this.products = []
+          // eslint-disable-next-line
+          this.products = store.state.products.filter((product: any) => {
+            if (product.id.toString().toLowerCase().indexOf(word) > -1) {
+              return true
+            } else if (product.title.toLowerCase().indexOf(word) > -1) {
+              return true
+            } else if (product.category.toLowerCase().indexOf(word) > -1) {
+              return true
+            } else if (product.description.toLowerCase().indexOf(word) > -1) {
+              return true
+            }
+            return false
+          });
+        } else {
+          this.products = store.state.products
+        }
+      }, 500);
     }
   }
 })
